a75ae81d6e1ab183a7406634a19bb9f2fce7ff7e
gracekight <gracekight@tamu.edu>
2024-09-23 08:39:06 -0500

    Basic Queries

--- answers.sh
+++ answers.sh
@@ -1,30 +1,12 @@
-SELECT Name FROM Students;
-SELECT * FROM Students WHERE Age > 30;
-SELECT Name FROM Students WHERE Gender='F' AND Age=30;
-SELECT Points FROM Students WHERE Name='Alex';
-INSERT INTO Students VALUES(10, 'Nabigha', '24', 'F', 500);
-UPDATE Students SET Points = 500 WHERE Name='Basma';
-UPDATE Students SET Points = 100 WHERE Name='Alex';
-
-CREATE TABLE graduates(
-    ID INTEGER  PRIMARY KEY AUTOINCREMENT,
-    Name  TEXT NOT NULL UNIQUE,
-    Age INTEGER,
-	Gender TEXT,
-	Points INTEGER,
-	Graduation TEXT
-);
-
-INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM Students WHERE Students.Name='Layal';
-UPDATE graduates SET Graduation='08-09-2018' WHERE Name='Layal';
-DELETE FROM graduates WHERE Name='Layal';
-
-SELECT employees.name, employees.Company, companies.date FROM employees Inner join companies ON employees.Company=companies.name;
-SELECT employees.name FROM employees Inner join companies ON employees.Company=companies.name and companies.Date < 2000;
-SELECT companies.name FROM companies Inner join employees ON employees.Company=companies.name and employees.Role='Graphic Designer';
-
-select name from students where points=(SELECT MAX(points) from students);
-select AVG(points) from students;
-select COUNT(Name) from students where points=500;
-select Name from students WHERE Name GLOB '*s*';
-select name from students ORDER BY points DESC;
+SELECT Name FROM students;
+SELECT * FROM students
+    WHERE age > 30;
+SELECT Name FROM students
+    WHERE age =30 AND gender='F';
+SELECT points FROM students 
+    WHERE name='Alex';
+INSERT INTO students VALUES(7, 'Grace', 21, 'F', 500);
+UPDATE students SET points=400 
+    WHERE name='Basma';
+UPDATE students SET points=100 
+    WHERE name='Alex';
