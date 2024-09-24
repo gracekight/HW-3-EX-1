e80f677771fa5b8604dd1887b4b5f4c15116e367
gracekight <gracekight@tamu.edu>
2024-09-24 11:07:35 -0500

    Creating Table

--- answers.sh
+++ answers.sh
@@ -10,3 +10,21 @@ UPDATE students SET points=400
     WHERE name='Basma';
 UPDATE students SET points=100 
     WHERE name='Alex';
+
+
+CREATE TABLE graduates(
+	ID INTEGER PRIMARY KEY AUTOINCREMENT,
+	Name TEXT NOT NULL UNIQUE,
+	Age INTEGER,
+	Gender TEXT,
+	Points INT,
+	Graduation TEXT);
+
+INSERT INTO graduates(Name, Age, Gender, Points)
+SELECT Name, Age, Gender, Points FROM students
+WHERE Name='Layal';
+UPDATE graduates SET graduation='08/09/2018' 
+WHERE Name='Layal';
+SELECT * FROM graduates;
+DELETE FROM students WHERE Name='Layal';
+SELECT * FROM students;
